0da4de7dada1010a9c908117bfa3b8936526a123
Bhavna Basenoo <bhavnabasenoo@yahoo.com>
2021-10-29 16:04:29 +0400

    watchlist

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :list
+    root to: 'lists#index'
+    resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
